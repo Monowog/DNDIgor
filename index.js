e261adb5cd2c7887a6295cd73aa2e91d093cfd0e
Jackson Cmelak <jackowen@gmail.com>
2026-01-28 14:55:55 -0800

    Combine deploy-commands.js into index.js

--- index.js
+++ index.js
@@ -1,8 +1,48 @@
 require('dotenv').config();
 const fs = require('node:fs');
 const path = require('node:path');
-const {Collection, Events} = require('discord.js');
-const {client } = require('./config/config');
+const {REST, Routes, Collection, Events} = require('discord.js');
+const {client} = require('./config/config');
+
+const token = process.env.TOKEN;
+const clientID = process.env.CLIENT_ID;
+const guildID = process.env.GUILD_ID;
+
+const commands = [];
+// Grab all the command folders from the commands directory
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+	// Grab all the command files from the commands directory
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith('.js'));
+	// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		if ('data' in command && 'execute' in command) {
+			commands.push(command.data.toJSON());
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
+}
+
+// Construct and prepare an instance of the REST module
+const rest = new REST().setToken(token);
+
+//deploy commands
+(async () => {
+	try {
+		console.log(`Started refreshing ${commands.length} application (/) commands.`);
+		//  refresh all commands in the guild with the current set
+		const data = await rest.put(Routes.applicationGuildCommands(clientID, guildID), { body: commands });
+		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+	} catch (error) {
+		console.error(error);
+	}
+})();
 
 const Database = require('better-sqlite3');
 
@@ -103,9 +143,6 @@ db.exec(`
 client.db = db;
 
 client.commands = new Collection(); 
-
-const foldersPath = path.join(__dirname, 'commands');
-const commandFolders = fs.readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
 	const commandsPath = path.join(foldersPath, folder);
